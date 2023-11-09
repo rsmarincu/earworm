b687b3eb57a7726723865a128db78f2efe30ae69
Robert <robert.marincu@grid.gg>
2023-11-09 18:19:46 +0200

    added create env

--- packages/web/src/middleware.ts
+++ packages/web/src/middleware.ts
@@ -1,6 +1,5 @@
 import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
-import { env } from "./env.mjs";
 
 export async function middleware(req: NextRequest) {
     const token = await getToken({ req })
