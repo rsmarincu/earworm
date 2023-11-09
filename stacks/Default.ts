664298c506f882d9709361a997027e49d5b8378d
Robert <robert.marincu@grid.gg>
2023-11-09 15:19:09 +0200

    deploying

--- stacks/Default.ts
+++ stacks/Default.ts
@@ -4,10 +4,10 @@ export function Default({ stack }: StackContext) {
   const site = new NextjsSite(stack, "site", {
     path: "packages/web",
     environment: {
-      "NEXTAUTH_URL": process.env.NEXTAUTH_URL,
-      "NEXTAUTH_SECRET": process.env.NEXTAUTH_SECRET,
-      "SPOTIFY_CLIENT_ID": process.env.SPOTIFY_CLIENT_ID,
-      "SPOTIFY_CLIENT_SECRET": process.env.SPOTIFY_CLIENT_SECRET
+      NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+      NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+      SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
+      SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET
     }
   });
 
