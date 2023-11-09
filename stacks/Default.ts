0477d2d982560b09f837ca534c00be8da0ebebed
Robert <robert.marincu@grid.gg>
2023-11-09 15:39:39 +0200

    added env mjs

--- stacks/Default.ts
+++ stacks/Default.ts
@@ -3,12 +3,6 @@ import { StackContext, NextjsSite } from "sst/constructs";
 export function Default({ stack }: StackContext) {
   const site = new NextjsSite(stack, "site", {
     path: "packages/web",
-    environment: {
-      NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-      NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-      SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
-      SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET
-    }
   });
 
   stack.addOutputs({
