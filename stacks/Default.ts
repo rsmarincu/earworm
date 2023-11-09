84f6d77b9fb5e257155e1a20fd0c284667822ccb
Robert <robert.marincu@grid.gg>
2023-11-09 18:08:24 +0200

    added middleware

--- stacks/Default.ts
+++ stacks/Default.ts
@@ -3,6 +3,12 @@ import { StackContext, NextjsSite } from "sst/constructs";
 export function Default({ stack }: StackContext) {
   const site = new NextjsSite(stack, "site", {
     path: "packages/web",
+    environment: {
+      SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
+      SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET,
+      NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+      NEXTAUTH_URL: process.env.NEXTAUTH_URL
+    }
   });
 
   stack.addOutputs({
